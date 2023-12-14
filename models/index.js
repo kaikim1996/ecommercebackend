d01dc71a9d9bec2002a22a9bc130131b37eebd0d
Kaitlyn Kim <kk4874a@student.american.edu>
2023-12-13 22:45:50 -0500

    updated routes

--- models/index.js
+++ models/index.js
@@ -4,13 +4,22 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongs To Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Product,{through: ProductTag});
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag});
 
 module.exports = {
   Product,
